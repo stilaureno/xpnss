ffe9d94c9959ce9e1f5aec84e9a42f6e57f67dae
MasteRuni <runiprime@gmail.com>
2026-05-03 17:27:28 +0800

    fix: remove conflicting routeTree.gen.ts file
    - Delete routeTree.gen.ts that's conflicting with routeTree.gen.tsx
    - Caused build failure on Netlify

--- src/routeTree.gen.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Route as rootRouteDef } from './routes/__root';
-import { Route as indexRouteDef } from './routes/index';
-
-export const routeTree = rootRouteDef.addChildren([indexRouteDef]);
-
-export const rootRoute = rootRouteDef;
-export const IndexRoute = indexRouteDef;
